50d15530456a76e97ee36c44a721efafd7995c78
ryanTavcar <ryan.tavcar@gmail.com>
2025-04-05 18:14:44 +1100

    chore: update window styles with custom title bar

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -15,6 +15,32 @@ namespace Names
             DataContext = new MainViewModel();
         }
 
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+                this.WindowState = WindowState.Normal;
+            else
+                this.WindowState = WindowState.Maximized;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Add this to enable dragging the window
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            if (e.ButtonState == MouseButtonState.Pressed)
+                this.DragMove();
+        }
+
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             UpdateSizeLabel();
