319f2992720db9e3b9b8e4eed44fc03b82de1db0
ryanTavcar <ryan.tavcar@gmail.com>
2025-04-07 21:47:20 +1000

    feat: added logivc methods for recording default hotkey

--- Views/SettingsPage.xaml.cs
+++ Views/SettingsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Names.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,20 @@ namespace Names.Views
     /// </summary>
     public partial class SettingsPage : Page
     {
+
+        private bool _showTooltips = true;
+        private bool _isRecordingHotKey = false;
+        private string _defaultRecordingHotKey = "LeftShift";
+        private bool _randomizeTimingCheckBox = false;
+
         public SettingsPage()
         {
             InitializeComponent();
+
+            // Altered default settings on initalization
+            ShowTooltips.IsChecked = _showTooltips;
+            RecordingHotkeyTextBox.Text = _defaultRecordingHotKey;
+            RandomizeTimingCheckBox.IsChecked = _randomizeTimingCheckBox;
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
@@ -32,6 +44,85 @@ namespace Names.Views
             {
                 mainWindow.ReturnToMain();
             }
+        }
+
+        private void ShowTooltipsCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            // Cast sender to CheckBox
+            CheckBox checkBox = sender as CheckBox;
+
+            if (checkBox != null)
+            {
+                bool isChecked = checkBox.IsChecked ?? false;
+
+                // Log to the centralized service with the state
+                LoggerService.Instance.Log($"Show tooltip {(isChecked ? "enabled" : "disabled")}");
+
+                // You can also use the value to update settings or behavior
+                 _showTooltips = isChecked;
+            }
+            e.Handled = true;
+        }
+
+        private void RecordingHotkeyTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            StartRecordingHotkey();
+        }        
+        
+        private void RecordHotkeyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isRecordingHotKey)
+            {
+                StopRecordingHotkey();
+            }
+            else
+            {
+                StartRecordingHotkey();
+            }
+        }
+
+        private void StartRecordingHotkey()
+        {
+            if (_isRecordingHotKey) return;
+            _isRecordingHotKey = true;
+            RecordingHotkeyTextBox.Text = "Press any key...";
+            RecordingHotkeyTextBox.Background = new SolidColorBrush(Color.FromRgb(60, 60, 60));
+            RecordHotkeyButton.Content = "Stop";
+
+            // Register the key event handler
+            this.PreviewKeyDown  += HotkeyRecording_PreviewKeyDown;
+
+            // Log to the centralized service with the state
+            LoggerService.Instance.Log("Press any key to set as Hotkey...");
+
+        }        
+        
+        private void StopRecordingHotkey()
+        {
+            if (!_isRecordingHotKey) return;
+            _isRecordingHotKey = false;
+            RecordingHotkeyTextBox.Background = new SolidColorBrush(Color.FromRgb(30, 30, 30));
+            RecordHotkeyButton.Content = "Record";
+
+            // Remove handler
+            this.PreviewKeyDown -= HotkeyRecording_PreviewKeyDown;
+        }
+
+        private void HotkeyRecording_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // Capture the key
+           string hotkey = e.Key.ToString();
+
+            // Stop recording
+            StopRecordingHotkey();
+
+            _defaultRecordingHotKey = hotkey;
+            RecordingHotkeyTextBox.Text = _defaultRecordingHotKey;
+
+            LoggerService.Instance.Log($"Trigger key set to: {hotkey}");
+
+            // Prevent the key from being processed further
+            e.Handled = true;
         }
     }
 }
